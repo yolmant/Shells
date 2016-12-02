f25edfe5dd0cf5e3a4d69d18bcbb4a83b811a397
Cloud User <ec2-user@ip-172-31-39-33.us-west-2.compute.internal>
2016-12-01 23:02:13 -0500

    git installer in python

--- Python/github.py
+++ Python/github.py
@@ -2,9 +2,9 @@
 import os
 
 print('installing git')
-os.system('sudo yum -y install git)
+os.system('sudo yum -y install git')
 
-url= raw_input('introduce you URL from github').lower()
-os.system('git clone '+url)
+Url= raw_input('introduce you URL from github ').lower()
+os.system('git clone '+Url)
 
 
